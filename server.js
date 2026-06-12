13bc43702ebbee194e889fb76a9f423715683e3f
ChocolateN2 <nankarsw80@email.com>
2026-06-12 17:02:59 -0300

    fix port 8080

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.get('/pago-pendiente', (req, res) => {
   `);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`✓ Servidor corriendo en puerto ${PORT}`);
   console.log(`✓ URL pública: ${BASE_URL}`);
